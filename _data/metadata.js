9faa5ac88424fb50b2cf49753639860cae5851ad
brad fisher <bradfisher@brads-MacBook-Pro.local>
2023-06-26 22:42:27 -0400

    first commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Sell My House Fast GA",
+	url: "https://storage.googleapis.com/",
+	language: "en",
+	description: "Real estate investment is my passion. Hi, Im Grant Garab. I am the proud co-owner of WeAreHomeBuyers.com. As a retired Marine, I devoted over sixteen years of my life to serving our great country. In 2019, my life took an unexpected turn when I was involved in a head-on collision with a texting teenager while riding my motorcycle home from work. This near-fatal accident profoundly altered my perspective on career goals for both myself and my family. While confined to a wheelchair for over eight months and undergoing more than a dozen surgeries, I used my downtime to contemplate the next chapter in my life, considering the future of my wife of fifteen years and our three sons. With retirement from the Marine Corps on the horizon and years of experience assisting homeowners with their unwanted properties as a side job, I decided to establish WeAreHomeBuyers.com. My primary objective is to buy and sell houses and properties in a way that benefits all parties involved. I am driven by a relentless determination to transform seemingly impossible situations into viable solutions. For me, the needs of my customers always come first. No matter the circumstance, my team and I are here to devise a strategy that addresses your real estate challenges. At WeAreHomeBuyers.com, we take pride in working closely with our clients and forging lasting friendships along the way. We are not just a company; we are a trusted partner dedicated to finding innovative solutions for your real estate needs. Why Work With Me? What you see is what you get. I am honest, fair, and reliable. I work HARD! I want to help each and every homeowner including you!",
+	author: {
+		name: "Grant Garab",
+		email: "grant@wearehomebuyers.com",
+		url: "https://storage.googleapis.com/sellmyhousefastga/about-me/"
+	}
+}
